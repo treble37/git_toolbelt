f4378cd6a21bf69398d8b278d29046fe463a51b5
Bruce Park <bruce@binarywebpark.com>
2014-05-26 21:00:28 -0700

    number of lines in a commit, hash, and message

--- git-stats.sh
+++ git-stats.sh
@@ -2,12 +2,29 @@
 
 set -e
 
-git rev-list --reverse HEAD |
-while read rev; do
-	git log --oneline -1 $rev
-	echo REV $rev;
-	git ls-tree -r $rev |
-	awk '{print $3}' |
-	xargs git show |
-	wc -l
-done
+# file_pattern = $1
+
+function main {
+  
+  for rev in `revisions`; do 
+    echo "`number_of_lines` `commit_description`"
+  done
+}
+
+function revisions {
+  git rev-list --reverse HEAD 
+}
+
+function number_of_lines {
+    git ls-tree -r $rev |
+    grep "$file_pattern" |
+    awk '{print $3}' |
+    xargs git show |
+    wc -l
+}
+
+function commit_description {
+  git log --oneline -1 $rev
+}
+
+main
